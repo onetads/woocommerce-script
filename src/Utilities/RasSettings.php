78e696904562ab6d7309320567bdcd5c6b52b783
Jakub <jakub.gloger@fingoweb.com>
2024-03-12 14:20:15 +0100

    check if woocommerce plugin is active

--- src/Utilities/RasSettings.php
+++ src/Utilities/RasSettings.php
@@ -6,4 +6,5 @@ class RasSettings
 {
     public const MIN_SUPPORTED_WOOCOMMERCE_VERSION = 8.5;
     public const MIN_SUPPORTED_WORDPRESS_VERSION = 6.3;
+    public const WOOCOMMERCE_CLASS_NAME = 'WooCommerce';
 }
