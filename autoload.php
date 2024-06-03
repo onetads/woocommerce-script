1ae8d9125f2a7be4b580c6bab78ab05ad3b3d6a6
Jakub Gloger <jakub.gloger@fingoweb.com>
2024-06-03 10:23:02 +0200

    remove require for BlockNotFound

--- autoload.php
+++ autoload.php
@@ -6,5 +6,4 @@ require_once $GLOBALS['RAS']['PATH'] . 'src/Utilities/RasSettings.php';
 require_once $GLOBALS['RAS']['PATH'] . 'src/Hooks/CheckVersionHook.php';
 require_once $GLOBALS['RAS']['PATH'] . 'src/Hooks/ProductHook.php';
 require_once $GLOBALS['RAS']['PATH'] . 'src/Utilities/ProductUtil.php';
-require_once $GLOBALS['RAS']['PATH'] . 'src/Exceptions/RasBlockNotFoundException.php';
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
