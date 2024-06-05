a03aad4e141d816ccca3def8618a8ac44d10e850
Patryk Papiór <patryk.papior@fingoweb.com>
2024-06-05 10:21:15 +0200

    Create endpoint for retrieving requried html parts

--- src/Hooks/ProductHook.php
+++ src/Hooks/ProductHook.php
@@ -3,6 +3,7 @@
 namespace Ras\Hooks;
 
 use Ras\Utilities\ProductUtil;
+use WP_Query;
 use WP_REST_Request;
 use WP_REST_Response;
 
@@ -26,6 +27,53 @@ class ProductHook
         }
 
         $productUtil->get_product_html();
+
+        exit();
+    }
+
+    public function return_required_html_elements(): void
+    {
+        $args = [
+            'post_type' => 'product',
+            'posts_per_page' => 1,
+            'orderby' => 'ID',
+            'post_status' => 'publish',
+            'meta_query' => [
+                [
+                    'key' => '_stock_status',
+                    'value' => 'instock',
+                ],
+            ],
+        ];
+
+        $query = new WP_Query($args);
+
+        $productId = null;
+
+        if ($query->have_posts()) {
+            $query->the_post();
+
+            global $product;
+
+            $productId = $product->get_id();
+        }
+
+        wp_reset_postdata();
+
+        if (is_null($productId)) {
+            wp_send_json([
+                'message' => 'No products found',
+            ], 404);
+        }
+
+        $productUtil = new ProductUtil($productId);
+
+        wp_send_json([
+            'list_container_html' => $productUtil->get_product_list_container_html(),
+            'product_tag' => $productUtil->get_product_tag(),
+            'link_html' => $productUtil->get_product_link_html(),
+            'promo_tag' => $productUtil->get_product_promo_tag(),
+        ]);
     }
 
 
--- src/Utilities/ProductUtil.php
+++ src/Utilities/ProductUtil.php
@@ -2,6 +2,8 @@
 
 namespace Ras\Utilities;
 
+use DOMDocument;
+use DOMXPath;
 use WC_Product;
 use WP_Post;
 
@@ -37,12 +39,70 @@ class ProductUtil
         return true;
     }
 
-    public function get_product_html() {
+    public function get_product_html(): void
+    {
         global $post;
 
         $post = get_post($this->product_id);
 
         $this->render_content_for_template($post);
+    }
+
+    public function get_product_list_container_html(): string
+    {
+        ob_start();
+
+        wc_get_template( 'loop/loop-start.php' );
+
+        return trim(ob_get_clean());
+    }
+
+    public function get_product_tag(): ?string
+    {
+        ob_start();
+
+        $this->get_product_html();
+
+        $productHtml = ob_get_clean();
+
+        $dom = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($productHtml);
+        libxml_clear_errors();
+
+        $xpath = new DOMXPath($dom);
+        $elements = $xpath->query("//*[contains(@class, 'post-')]");
+
+        $productTag = null;
+        foreach ($elements as $element) {
+            $classes = explode(' ', $element->getAttribute('class'));
+            foreach ($classes as $class) {
+                if (preg_match('/^post-\d+$/', $class)) {
+                    $productTag = $element->nodeName;
+                    break;
+                }
+            }
+        }
+
+        return $productTag;
+    }
+
+    public function get_product_link_html(): string
+    {
+        ob_start();
+
+        woocommerce_template_loop_product_link_open();
+
+        return trim(ob_get_clean());
+    }
+
+    public function get_product_promo_tag(): string
+    {
+        ob_start();
+
+        woocommerce_show_product_loop_sale_flash();
+
+        return trim(ob_get_clean());
     }
 
     /**
@@ -58,7 +118,5 @@ class ProductUtil
         wc_get_template_part(self::TEMPLATE_PART_SLUG, self::TEMPLATE_PART_NAME);
 
         wp_reset_postdata();
-
-        exit();
     }
 }
